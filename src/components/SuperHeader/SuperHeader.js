19eb68c2568a810f1a668218d9e5e4189e5796be
Billy Reilly <billy_reilly@hotmail.co.uk>
2021-06-19 20:15:22 +0100

    1) superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import Spacer from "../Spacer";
 
 const SuperHeader = () => {
   return (
@@ -13,23 +14,36 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      <AlignRight>
+        <SearchInput />
+        <Spacer size={24} />
+        <HelpLink href="/help">Help</HelpLink>
+        <Spacer size={24} />
+        <UnstyledButton display="inline-block">
+          <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
+      </AlignRight>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  padding: 0 32px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
+  padding: 12px 0;
   color: ${COLORS.white};
+`;
+
+const AlignRight = styled.div`
+  display: flex;
 `;
 
 const HelpLink = styled.a`
